da06cad6098e6c7295155499d90989cb5810d124
Brock Beaudry <brock.beaudry@gmail.com>
2014-08-11 18:23:21 -0700

    Add server.js with initial server setup

---
+++ server.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// Dependencies
+
+var http = require('http');
+var express = require('express');
+
+// Dependency instances
+
+var app = express();
+var server = http.createServer(app);
+
+// Server config
+
+app.set('port', process.env.PORT || 3000);
+app.listen(app.get('port'), function() {
+    console.log('Listening on port: %d', app.get('port'));
+});
