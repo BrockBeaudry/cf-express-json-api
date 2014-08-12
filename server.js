5758a87265862a115875298b78dcefe200436ed8
Brock Beaudry <brock.beaudry@gmail.com>
2014-08-11 18:40:23 -0700

    Add /name route for greeting the user

--- server.js
+++ server.js
@@ -16,3 +16,9 @@ app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'), function() {
     console.log('Listening on port: %d', app.get('port'));
 });
+
+// Routing
+
+app.get('/name/:name', function(request, response) {
+    response.json({ msg: 'Hello, ' + request.param('name') });
+});
